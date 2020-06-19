0c71add108b0084a2a136eba5fe6b0f9c0b5a33d
Purnachander <ramagiri.purnachander@gmail.com>
2020-06-19 12:51:45 +0530

    toaster

--- toasterlibrary/src/main/java/com/sparsh/toasterlibrary/ToasterMessage.java
+++ toasterlibrary/src/main/java/com/sparsh/toasterlibrary/ToasterMessage.java
@@ -6,7 +6,7 @@ import android.widget.Toast;
 public class ToasterMessage {
     public static void s(Context c, String message){
 
-        Toast.makeText(c,message, Toast.LENGTH_SHORT).show();
+        Toast.makeText(c,"message", Toast.LENGTH_SHORT).show();
 
     }
 }
